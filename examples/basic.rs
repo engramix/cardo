c856a03258b860903ad54bb55297648b7f067ec3
Svet Penkov <svet@efemarai.com>
2026-02-09 13:15:47 +0200

    Update basic example

--- examples/basic.rs
+++ examples/basic.rs
@@ -2,14 +2,17 @@ use cardo::prelude::*;
 
 struct World;
 struct Body;
+struct Link;
 struct Camera;
 
 fn main() {
-    let camera_to_body: SO3<Camera, Body> = SO3::identity();
+    let camera_to_link: SO3<Camera, Link> = SO3::identity();
+    let link_to_body: SO3<Link, Body> = SO3::identity();
     let body_to_world: SO3<Body, World> = SO3::identity();
 
-    // Chain transformations: Camera -> Body -> World
-    let camera_to_world: SO3<Camera, World> = chain!(camera_to_body -> body_to_world);
+    // Chain transformations: Camera -> Link -> Body -> World
+    let camera_to_world: SO3<Camera, World> =
+        chain!(camera_to_link -> link_to_body -> body_to_world);
 
     let v: Vector3<Camera> = Vector3::new(1.0, 0.0, 0.0);
     let w: Vector3<World> = camera_to_world * v;
