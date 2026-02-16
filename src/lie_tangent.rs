461ecc71d64a60bbea6edc88b80c5b9422741923
Svet Penkov <svet@efemarai.com>
2026-02-16 17:02:44 +0200

    Make sure primitive functions are implemented by a groups/tangents

--- src/lie_tangent.rs
+++ src/lie_tangent.rs
@@ -5,6 +5,23 @@
 ///   impl_lie_tangent!(Tangent = SE3Tangent, AdjMat = Mat6);
 macro_rules! impl_lie_tangent {
     (Tangent = $Tangent:ident, AdjMat = $AdjMat:ident) => {
+        // Compile-time check that all tangent primitives are implemented.
+        // This function is never called; the compiler only verifies signatures.
+        #[doc(hidden)]
+        const _: () = {
+            #[allow(unused, unreachable_code, clippy::diverging_sub_expression)]
+            fn _assert_tangent_primitives<A, B, C, T: Float>(
+                t: $Tangent<A, B, C, T>,
+                t_exp: $Tangent<A, B, A, T>,
+            ) {
+                let _ = t.hat();
+                let _: $AdjMat<T> = t.ad();
+                let _: $AdjMat<T> = t.ljac();
+                let _: $AdjMat<T> = t.ljacinv();
+                let _ = t_exp.exp();
+            }
+        };
+
         impl<A, B, C, T: Float> $Tangent<A, B, C, T> {
             pub fn rjac(&self) -> $AdjMat<T> {
                 (-*self).ljac()
